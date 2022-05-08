dd91b9a3e7a1e6177b3d7a896ad62f68d247de1a
luit saikia <luit93@gmail.com>
2022-05-09 04:57:25 +1000

    setting up mongo commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 
-
+//dependencies
 const express = require('express')
 const app = express()
 const mongoose = require("mongoose")
+const dotenv = require('dotenv')
 
-app.listen(5050,()=>{
+dotenv.config()
+
+//connect to mongo db
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('Database connection successful')).catch((err)=>{console.log(err)})
+
+//testing
+app.get('/api/test',()=>{
+    console.log('testing ....')
+})
+app.listen(process.env.PORT || 7000,()=>{
     console.log('backend server is running')
 })
