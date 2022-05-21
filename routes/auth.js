10d95deb325f732d277d10883c4327e60f8cc1c9
luit saikia <luit93@gmail.com>
2022-05-22 06:16:56 +1000

    register and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/Users");
+const CryptoJS = require("crypto-js");
 
 //Register
 
@@ -7,7 +8,10 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
@@ -17,5 +21,49 @@ router.post("/register", async (req, res) => {
     res.status(500).json(error);
   }
 });
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if(!user) {
+        res.status(401).json("Wrong credentials")
+        return
+    }
 
+    else{
+        console.log("username exists")
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+          );
+          const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+      
+          if(originalPassword !== req.body.password ){
+              console.log('user.username : '+user.username)
+              console.log('user.email : '+user.email)
+              console.log("___ __ __-- ____ ---")
+              console.log('user.password : '+user.password)
+              console.log('req.body.password : '+req.body.password)
+              
+              console.log('originalPassword : '+originalPassword)
+              console.log('hashedPassword : '+hashedPassword)
+              
+             
+              res.status(401).json("Wrong credentials2")
+              return
+          }
+          else{
+              console.log("woohoooooooo")
+              const {password, ...others}= user._doc
+            res.status(200).json(others);
+
+          }
+      
+      
+    }
+  
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
